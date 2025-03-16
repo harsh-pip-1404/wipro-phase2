50b35469f73d5f8ff5e2f889d85315222e1ccafa
harsh <harshfcl@gmail.com>
2025-03-16 21:31:16 +0530

    Added Day5 folder

---
+++ Day5/P2Day5Projects/EntityFrameworkDemo1/Models/CustomerRange.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityFrameworkDemo1.Models
+{
+    public class CustomerRange
+    {
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; } = string.Empty;
+        public int? orderscount { get; set; }
+    }
+}
---
+++ Day5/P2Day5Projects/EntityFrameworkDemo1/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkDemo1.Models;
+
+public partial class Post
+{
+    public int PostId { get; set; }
+
+    public DateTime? DatePublished { get; set; }
+
+    public string? Body { get; set; }
+
+    public string? Title { get; set; }
+}
