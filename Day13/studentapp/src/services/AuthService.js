5bfdc1c6a00adca28534e94ca01ece38bf2c6e4c
harsh <harshfcl@gmail.com>
2025-03-16 21:44:54 +0530

    Added Day13 folder

---
+++ Day13/studentapp/src/services/AuthService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:7139/api/Auth';
+
+class AuthService {
+    register(userData) {
+        return axios.post(`${API_URL}?role=${userData.role}`, userData);
+        // Sending role as query parameter instead of in request body
+    }
+
+    login(userData) {
+        return axios.post(API_URL + '/login', userData)
+            .then(response => {
+                if (response.data.token) {
+                    localStorage.setItem('user', JSON.stringify(response.data));
+                }
+                return response.data;
+            });
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));
+    }
+}
+
+export default new AuthService();
