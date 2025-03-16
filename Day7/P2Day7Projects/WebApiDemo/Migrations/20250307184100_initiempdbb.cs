0c4ffd60a25c9b0fe6a372901dbc3b15844564cf
harsh <harshfcl@gmail.com>
2025-03-16 21:40:16 +0530

    Added Day7 folder

---
+++ Day7/P2Day7Projects/WebApiDemo/Migrations/20250307184100_initiempdbb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class initiempdbb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
